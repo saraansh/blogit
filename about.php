63034dac1cb07870fc2f3238a7edcd99155de65a
saraansh <bsaraansh@gmail.com>
2016-12-06 16:27:05 +0530

    Information on Developers

---
+++ about.php
@@ -0,0 +1,10 @@
+<h4>The developers of BlogIt [Batch-B1]:</h4>
+<h5>
+<br>
+<ul>
+	<li>Group Coordinator => Saraansh Baranwal	(15103022)</li><br>
+	<li>Frontend Designer => Rupam Saxena		(15103019)</li><br>
+	<li>Content - Manager => Akshay Paliwal		(15103006)</li><br>
+	<li>Backend Designer => Tamanna Sharma		(15103027)</li>
+</ul>
+</h5>
